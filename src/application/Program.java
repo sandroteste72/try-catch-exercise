54564621be5dd9a807233bc15fac73526e79921f
Sandro Arpi <sandro.teste72@gmail.com>
2021-04-11 00:13:57 -0300

    Complete exercise

---
+++ src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = scan.nextInt();
+		System.out.print("Holder: ");
+		scan.nextLine();
+		String holder = scan.nextLine();
+		System.out.print("Initial balance: ");
+		Double balance = scan.nextDouble();
+		System.out.print("Withdraw limit: ");
+		Double withdrawLimit = scan.nextDouble();
+		Account account = new Account(number, holder, balance, withdrawLimit);
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		Double amount = scan.nextDouble();
+		try {
+			account.withdraw(amount);
+
+		} catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+
+		scan.close();
+	}
+
+}
